8694a115b5ba977c4f6244be2d35fc428db6cc04
krishna4a6av <kkrryadav1123@gmail.com>
2025-10-01 16:03:19 +0530

    updated themes

--- Scripts/Colors.sh
+++ Scripts/Colors.sh
@@ -5,10 +5,12 @@
 THEME_DIR="$HOME/.config/colors/themes"
 ROFI_DIR="$HOME/.config/colors/rofi"
 HYPR_DIR="$HOME/.config/colors/hyprland"
+KITTY_DIR="$HOME/.config/colors/kitty"
 
 THEME_LINK="$HOME/.config/colors/theme.css"
 ROFI_LINK="$HOME/.config/colors/rofi_theme.rasi"
 HYPR_LINK="$HOME/.config/colors/colors.conf"
+KITTY_LINK="$HOME/.config/colors/colors-kitty.conf"
 
 THEME_NAME="$1"
 
@@ -20,6 +22,7 @@ fi
 THEME_FILE="$THEME_DIR/${THEME_NAME}.css"
 ROFI_FILE="$ROFI_DIR/${THEME_NAME}.rasi"
 HYPR_FILE="$HYPR_DIR/${THEME_NAME}.conf"
+KITTY_FILE="$KITTY_DIR/${THEME_NAME}.conf"
 
 # Check required files exist
 if [[ ! -f "$THEME_FILE" ]]; then
@@ -33,7 +36,12 @@ if [[ ! -f "$ROFI_FILE" ]]; then
 fi
 
 if [[ ! -f "$HYPR_FILE" ]]; then
-    echo "❌ Theme conf '$HYPR_FILE' not found."
+    echo "❌ Hyprland theme '$HYPR_FILE' not found."
+    exit 1
+fi
+
+if [[ ! -f "$KITTY_FILE" ]]; then
+    echo "❌ Kitty theme '$KITTY_FILE' not found."
     exit 1
 fi
 
@@ -41,8 +49,12 @@ fi
 ln -sf "$THEME_FILE" "$THEME_LINK"
 ln -sf "$ROFI_FILE" "$ROFI_LINK"
 ln -sf "$HYPR_FILE" "$HYPR_LINK"
+ln -sf "$KITTY_FILE" "$KITTY_LINK"
 
 # Reload components
 pkill -SIGUSR2 waybar
 pkill swaync && swaync & disown
+
+# Reload Kitty (will affect new windows only)
+kill -SIGUSR1 $(pgrep kitty)
 
